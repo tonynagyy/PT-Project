3ed62f17a3154a2d6c445eab63d8164e33974550
tonynagyy <nagytony2@gmail.com>
2023-11-13 15:46:14 +0200

    Add project files.

---
+++ GUI/Output.cpp
@@ -0,0 +1,281 @@
+#include "Output.h"
+
+
+Output::Output()
+{
+	//Initialize user interface parameters
+	UI.InterfaceMode = MODE_DRAW;
+	
+	UI.width = 1250;
+	UI.height = 650;
+	UI.wx = 5;
+	UI.wy =5;
+
+	
+	UI.StatusBarHeight = 50;
+	UI.ToolBarHeight = 50;
+	UI.MenuItemWidth = 80;
+	
+	UI.DrawColor = BLUE;	//Drawing color
+	UI.FillColor = GREEN;	//Filling color
+	UI.MsgColor = RED;		//Messages color
+	UI.BkGrndColor = LIGHTGOLDENRODYELLOW;	//Background color
+	UI.HighlightColor = MAGENTA;	//This color should NOT be used to draw figures. use if for highlight only
+	UI.StatusBarColor = TURQUOISE;
+	UI.PenWidth = 3;	//width of the figures frames
+
+	
+	//Create the output window
+	pWind = CreateWind(UI.width, UI.height, UI.wx, UI.wy);
+	//Change the title
+	pWind->ChangeTitle("Paint for Kids - Programming Techniques Project");
+	
+	CreateDrawToolBar();
+	CreateStatusBar();
+}
+
+
+Input* Output::CreateInput() const
+{
+	Input* pIn = new Input(pWind);
+	return pIn;
+}
+
+//======================================================================================//
+//								Interface Functions										//
+//======================================================================================//
+
+window* Output::CreateWind(int w, int h, int x, int y) const
+{ 
+	window* pW = new window(w, h, x, y);
+	pW->SetBrush(UI.BkGrndColor);
+	pW->SetPen(UI.BkGrndColor, 1);
+	pW->DrawRectangle(0, UI.ToolBarHeight, w, h);	
+	return pW;
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::CreateStatusBar() const
+{
+	pWind->SetPen(UI.StatusBarColor, 1);
+	pWind->SetBrush(UI.StatusBarColor);
+	pWind->DrawRectangle(0, UI.height - UI.StatusBarHeight, UI.width, UI.height);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::ClearStatusBar() const
+{
+	//Clear Status bar by drawing a filled white rectangle
+	pWind->SetPen(UI.StatusBarColor, 1);
+	pWind->SetBrush(UI.StatusBarColor);
+	pWind->DrawRectangle(0, UI.height - UI.StatusBarHeight, UI.width, UI.height);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::CreateDrawToolBar() const
+{
+	UI.InterfaceMode = MODE_DRAW;
+
+	//You can draw the tool bar icons in any way you want.
+	//Below is one possible way
+	
+	//First prepare List of images for each menu item
+	//To control the order of these images in the menu, 
+	//reoder them in UI_Info.h ==> enum DrawMenuItem
+	string MenuItemImages[DRAW_ITM_COUNT];
+	MenuItemImages[ITM_RECT] = "images\\MenuItems\\Menu_Rect.jpg";
+	
+	MenuItemImages[ITM_EXIT] = "images\\MenuItems\\Menu_Exit.jpg";
+
+
+
+	//Draw menu item one image at a time
+	for(int i=0; i<DRAW_ITM_COUNT; i++)
+		pWind->DrawImage(MenuItemImages[i], i*UI.MenuItemWidth, 0, UI.MenuItemWidth, UI.ToolBarHeight);
+		
+
+
+	//Draw a line under the toolbar
+	pWind->SetPen(RED, 3);
+	pWind->DrawLine(0, UI.ToolBarHeight, UI.width, UI.ToolBarHeight);	
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void Output::CreatePlayToolBar() const
+{
+	UI.InterfaceMode = MODE_PLAY;
+	///TODO: write code to create Play mode menu
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void Output::ClearDrawArea() const
+{
+	pWind->SetPen(UI.BkGrndColor, 1);
+	pWind->SetBrush(UI.BkGrndColor);
+	pWind->DrawRectangle(0, UI.ToolBarHeight, UI.width, UI.height - UI.StatusBarHeight);
+	
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void Output::PrintMessage(string msg) const	//Prints a message on status bar
+{
+	ClearStatusBar();	//First clear the status bar
+	
+	pWind->SetPen(UI.MsgColor, 50);
+	pWind->SetFont(20, BOLD , BY_NAME, "Arial");   
+	pWind->DrawString(10, UI.height - (int)(UI.StatusBarHeight/1.5), msg);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+color Output::getCrntDrawColor() const	//get current drawing color
+{	return UI.DrawColor;	}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+color Output::getCrntFillColor() const	//get current filling color
+{	return UI.FillColor;	}
+//////////////////////////////////////////////////////////////////////////////////////////
+	
+int Output::getCrntPenWidth() const		//get current pen width
+{	return UI.PenWidth;	}
+
+//======================================================================================//
+//								Figures Drawing Functions								//
+//======================================================================================//
+ 
+//DRAW RECTANGLE FUNCTION
+void Output::DrawRect(Point P1, Point P2, GfxInfo RectGfxInfo, bool selected) const
+{
+	color DrawingClr;
+	if(selected)	
+		DrawingClr = UI.HighlightColor; //Figure should be drawn highlighted
+	else			
+		DrawingClr = RectGfxInfo.DrawClr;
+	
+	pWind->SetPen(DrawingClr,1);
+	drawstyle style;
+	if (RectGfxInfo.isFilled)	
+	{
+		style = FILLED;		
+		pWind->SetBrush(RectGfxInfo.FillClr);
+	}
+	else	
+		style = FRAME;
+
+	
+	pWind->DrawRectangle(P1.x, P1.y, P2.x, P2.y, style);
+	
+}
+
+//DRAWING SQUARE FUNCTION
+void Output::Drawsquare(Point P1, GfxInfo RectGfxInfo, bool selected) const
+{
+	color DrawingClr;
+	if (selected)
+		DrawingClr = UI.HighlightColor; //Figure should be drawn highlighted
+	else
+		DrawingClr = RectGfxInfo.DrawClr;
+
+	pWind->SetPen(DrawingClr, 1);
+	drawstyle style;
+	if (RectGfxInfo.isFilled)
+	{
+		style = FILLED;
+		pWind->SetBrush(RectGfxInfo.FillClr);
+	}
+	else
+		style = FRAME;
+
+	int i = 50;
+	pWind-> DrawRectangle( P1.x-i, P1.y-i, P1.x+i, P1.y+i, style);
+
+}
+
+//DRAWING TRIANGLE FUNCTION
+void Output::Drawtriangle(Point P1 , Point P2 , Point P3 , GfxInfo RectGfxInfo, bool selected) const
+{
+	color DrawingClr;
+	if (selected)
+		DrawingClr = UI.HighlightColor; //Figure should be drawn highlighted
+	else
+		DrawingClr = RectGfxInfo.DrawClr;
+
+	pWind->SetPen(DrawingClr, 1);
+	drawstyle style;
+	if (RectGfxInfo.isFilled)
+	{
+		style = FILLED;
+		pWind->SetBrush(RectGfxInfo.FillClr);
+	}
+	else
+		style = FRAME;
+
+	
+	pWind->DrawTriangle( P1.x , P1.y , P2.x , P2.y , P3.x , P3.y , style);
+	
+}
+
+
+
+//DRAW HEXAGON FUNCTION
+void Output::Drawhexagon(Point P1, GfxInfo RectGfxInfo, bool selected) const
+{
+	int xvert[6] = { P1.x - 40 , P1.x - 40 , P1.x , P1.x + 40 , P1.x + 40 , P1.x };
+	int yvert[6] = { P1.y + 40 , P1.y - 40 , P1.y - 80 , P1.y - 40 , P1.y + 40 , P1.y + 80 };
+
+
+	color DrawingClr;
+	if (selected)
+		DrawingClr = UI.HighlightColor; //Figure should be drawn highlighted
+	else
+		DrawingClr = RectGfxInfo.DrawClr;
+
+	pWind->SetPen(DrawingClr, 1);
+	drawstyle style;
+	if (RectGfxInfo.isFilled)
+	{
+		style = FILLED;
+		pWind->SetBrush(RectGfxInfo.FillClr);
+	}
+	else
+		style = FRAME;
+
+	int i = 30;
+	pWind-> DrawPolygon ( xvert, yvert , 6 , style);
+
+}
+
+
+
+//DRAW CIRCLE FUNCTION
+void Output::Drawcircle ( Point P1 , Point P2 , GfxInfo RectGfxInfo , bool selected ) const
+{
+	color DrawingClr;
+	if (selected)
+		DrawingClr = UI.HighlightColor; //Figure should be drawn highlighted
+	else
+		DrawingClr = RectGfxInfo.DrawClr;
+
+	pWind-> SetPen(DrawingClr, 1);
+	drawstyle style;
+	if (RectGfxInfo.isFilled)
+	{
+		style = FILLED;
+		pWind->SetBrush(RectGfxInfo.FillClr);
+	}
+	else
+		style = FRAME;
+
+	Point diff;   //the difference between the two points 
+	diff.x = P2.x - P1.x;
+	diff.y = P2.y - P1.y; 
+
+	int  radius = sqrt ( pow( diff.x , 2 ) + pow ( diff.y , 2 ) ); // calculating the radius of circle
+
+	pWind-> DrawCircle  ( P1.x , P1.y , radius , style ) ;
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+Output::~Output()
+{
+	delete pWind;
+}
+
