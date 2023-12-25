be0ee20724d6ff52c0e59f20578c46b80ef6f6b5
Mario Raafat <mariorafat10@gmail.com>
2023-12-25 21:12:55 +0200

    Ya rb

--- Actions/AddRectAction.cpp
+++ Actions/AddRectAction.cpp
@@ -5,25 +5,32 @@
 #include "..\GUI\input.h"
 #include "..\GUI\Output.h"
 
-AddRectAction::AddRectAction(ApplicationManager* pApp, bool voice) :Action(pApp), rect(NULL), P1(0, 0), P2(0, 0), PlayRectVoice(voice)
-{
-}
-AddRectAction::AddRectAction(ApplicationManager *pApp) : Action(pApp) , rect(NULL), P1(0,0), P2(0,0)
+AddRectAction::AddRectAction(ApplicationManager* pApp, bool voice) :
+	Action(pApp), PlayRectVoice(voice), rect(NULL), P1(0, 0), P2(0, 0)
 {
 }
 
-void AddRectAction::ReadActionParameters() 
-{	
+AddRectAction::AddRectAction(const AddRectAction& other)
+	:Action(other), PlayRectVoice(other.PlayRectVoice), rect(NULL),
+	P1(other.P1), P2(other.P2), RectGfxInfo(other.RectGfxInfo)
+{
+	if (other.rect != NULL)
+	{
+		//rect = new CRectangle(*othjer.rect);
+		rect = static_cast<CRectangle*>(other.rect->clone());
+	}
+}
+
+void AddRectAction::ReadActionParameters()
+{
 	Output* pOut = pManager->GetOutput();
 	Input* pIn = pManager->GetInput();
 
 	if (PlayRectVoice)          // if Voice is enabled then play the voice of rectangle
 	{
-		PlaySound(("Voices\Rectvoice.wav"), NULL, SND_SYNC);
-		//PlaySound(0, 0, 0);
-
+		PlaySound(TEXT("Voices\\Rectvoice.wav.wav"), NULL, SND_ASYNC);
 	}
-	 
+
 	pOut->PrintMessage("New Rectangle: Click at first corner");
 	pIn->GetPointClicked(P1.x, P1.y);
 	pOut->PrintMessage("New Rectangle: Click at second corner");
@@ -37,16 +44,17 @@ void AddRectAction::ReadActionParameters()
 	pOut->ClearStatusBar();
 }
 
-void AddRectAction::Execute() 
+void AddRectAction::Execute()
 {
 	if (P1.x == 0 && P1.y == 0 && P2.x == 0 && P2.y == 0)
 	{
 		ReadActionParameters();
 	}
-	
+
 	if (rect == NULL)
-	{
-		 rect = new CRectangle(P1, P2, RectGfxInfo);
+	{// if rect == null then we need to make a new one bcs initally it is null
+		rect = new CRectangle(P1, P2, RectGfxInfo);
+		//sice rect is something new so we need to make a copy constructor
 	}
 
 	pManager->AddFigure(rect);
